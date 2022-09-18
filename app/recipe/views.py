153bee2c97bddb8e32da5fcf3e225e2ffe28ba16
Amir <amirzare309@yahoo.com>
2022-09-18 12:00:29 +0430

    implemented image api.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,9 +1,11 @@
 """
 Views for the recipe APIs.
 """
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from drf_spectacular.utils import (
     extend_schema_view,
@@ -63,12 +65,32 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return the serializer class for request."""
         if self.action == 'list':
             return serializers.RecipeSerializer
+        elif self.action == 'upload_image':
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['POST'], url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data=serializer.data,
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            data=serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 @extend_schema_view(
